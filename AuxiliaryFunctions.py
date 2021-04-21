b9c352e2764e0267d753d4aff313015e7fb03c4f
Shir Bar <63155990+shir3bar@users.noreply.github.com>
2021-04-22 00:17:52 +0300

    removed redundant roc-curve function

--- AuxiliaryFunctions.py
+++ AuxiliaryFunctions.py
@@ -200,30 +200,9 @@ def write_gif_fish(recon,directory,samp_id, heatmap=False):
     clip = ImageSequenceClip(list(frames), fps=20)
     clip.write_gif(os.path.join(directory,f'l1_recon_{samp_id}'), fps=10, verbose=False, logger=None)
 
-def roc_curve(errors, labels):
-    # a label of 1 is anomaly, a label of 0 is normal,
-    # errors are the average reconstruction error for each sample, they'll be our normality score for now
-    max_error = max(errors)  # get max error
-    tpr = np.zeros(len(errors))
-    fpr = np.zeros(len(errors))
-    precision = np.zeros(len(errors))
-    threshes = np.zeros(len(errors))
-    for i, thres in enumerate(np.linspace(0, max_error, len(errors))):
-    # iterate over the space of possible thresholds
-        new_labels = errors > thres  # get the new labels
-        tp = sum((new_labels + labels) == 2)
-        fn = new_labels[(new_labels != labels) & (new_labels == 0)].size
-        fp = new_labels[(new_labels != labels) & (new_labels == 1)].size
-        tn = sum((new_labels + labels) == 0)
-        precision[i] = tp / (tp + fp)
-        # Recall = tpr
-        tpr[i] = tp / (tp + fn)
-        fpr[i] = fp / (fp + tn)
-        threshes[i] = thres
-    auc_score = auc(fpr, tpr)
-    return auc_score,precision,fpr,tpr
 
-def roc_plots(auc_score,tpr,precision,save_dir='',save=True):
+
+def roc_plots(auc_score,fpr,tpr,precision,save_dir='',save=True):
     plt.figure(figsize=(15, 5))
     plt.subplot(1, 2, 1)
     plt.plot(fpr, tpr)
